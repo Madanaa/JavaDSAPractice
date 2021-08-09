778932d7138738be60ed7f90bf9ef8f1dd37cda1
Madan <58905767+Madanaa@users.noreply.github.com>
2021-08-09 10:29:12 -0700

    Create Swap2.java

---
+++ day_2/Swap2.java
@@ -0,0 +1,14 @@
+package day_2;
+
+public class Swap2 {
+
+	public static void main(String[] args) {
+		int a = 10;
+		int b = 12;
+		a =a - b;
+		b =b - a;
+		a = a + b;
+		System.out.println(a);
+	}
+
+}
