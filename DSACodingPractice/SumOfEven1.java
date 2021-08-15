7297cb459544b5200a815a83198ae9cacaeff7e2
Madan <58905767+Madanaa@users.noreply.github.com>
2021-08-15 10:04:07 -0700

    Create SumOfEven1.java

---
+++ DSACodingPractice/SumOfEven1.java
@@ -0,0 +1,12 @@
+package day_6;
+
+public class SumOfEven1 {
+	//t.c. o(1)
+            static int SumEven(int n) {
+            	return n*(n+1);
+            }
+	public static void main(String[] args) {
+		System.out.println(SumEven(4));
+	}
+
+}
